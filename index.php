fa25c5888e1243e30ca0561c5e191fd3c74b02ab
Emir Balic <emirbalic@gmail.com>
2018-12-15 12:51:58 +0100

    style index

--- index.php
+++ index.php
@@ -17,10 +17,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Momeer</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
      <div id="nowPlayingBarContainer">
+         <div id="nowPlayingBar">
+            <div id="nowPlayingLeft">
+
+            </div>
+            <div id="nowPlayingCenter">
+
+            </div>
+            <div id="nowPlayingRight">
+
+            </div>
+         </div>
 </div>
 </body>
 </html>
