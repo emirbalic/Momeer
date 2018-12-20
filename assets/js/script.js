d325c3616b369fc3aeb8f2e93cd546432bf8f6da
Emir Balic <emirbalic@gmail.com>
2018-12-20 02:02:27 +0100

    add update password

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,203 +10,239 @@ var userLoggedIn;
 var timer;
 
 // hide when clicked out of
-$(document).click((click)=>{
-    var target = $(click.target);
+$(document).click(click => {
+  var target = $(click.target);
 
-    if(!target.hasClass("item") && !target.hasClass("optionsButton")) {
-        hideOptionsMenu();
-    }
+  if (!target.hasClass('item') && !target.hasClass('optionsButton')) {
+    hideOptionsMenu();
+  }
 });
 
 //150 - hide when scroll
 $(window).scroll(() => {
-    hideOptionsMenu();
+  hideOptionsMenu();
 });
 
 //154 select the id of the song for a playlist when clicked on select tag
-$(document).on("change", "select.playlist", function(){
-    var select = $(this);
-    var playlistId = select.val(); //maybe not going to work with arrow func?? yes, it would not...
-    var songId = select.prev(".songId").val(); //prev takes the immidiate ancestor
+$(document).on('change', 'select.playlist', function() {
+  var select = $(this);
+  var playlistId = select.val(); //maybe not going to work with arrow func?? yes, it would not...
+  var songId = select.prev('.songId').val(); //prev takes the immidiate ancestor
 
-    $.post("includes/handlers/ajax/addToPlaylist.php", {playlistId: playlistId, songId:songId})
-    .done(function(error){
-        if(error != "") {
-            alert(error);
-            return;
-        }
-       hideOptionsMenu();
-       select.val(""); 
-    });
-    
+  $.post('includes/handlers/ajax/addToPlaylist.php', {
+    playlistId: playlistId,
+    songId: songId
+  }).done(function(error) {
+    if (error != '') {
+      alert(error);
+      return;
+    }
+    hideOptionsMenu();
+    select.val('');
+  });
 });
 function updateEmail(emailClass) {
-    var emailValue = $("." + emailClass).val();
+  var emailValue = $('.' + emailClass).val();
 
-    $.post("includes/handlers/ajax/updateEmail.php", {email:emailValue, username:userLoggedIn})
-    .done(function(response){
-        $("." + emailClass).nextAll(".message").text(response);
-    });
+  $.post('includes/handlers/ajax/updateEmail.php', {
+    email: emailValue,
+    username: userLoggedIn
+  }).done(function(response) {
+    $('.' + emailClass)
+      .nextAll('.message')
+      .text(response);
+  });
 }
 
-function logout () {
-    $.post("includes/handlers/ajax/logout.php", function(){
-        location.reload();
-    });
+function updatePassword(
+  oldPasswordClass,
+  newPasswordClass1,
+  newPasswordClass2
+) {
+  var oldPassword = $('.' + oldPasswordClass).val();
+  var newPassword1 = $('.' + newPasswordClass1).val();
+  var newPassword2 = $('.' + newPasswordClass2).val();
+
+  $.post('includes/handlers/ajax/updatePassword.php', {
+    oldPassword: oldPassword,
+    newPassword1: newPassword1,
+    newPassword2: newPassword2,
+    username:userLoggedIn
+  }).done(function(response) {
+    $('.' + oldPasswordClass)
+      .nextAll('.message')
+      .text(response);
+  });
+}
+
+function logout() {
+  $.post('includes/handlers/ajax/logout.php', function() {
+    location.reload();
+  });
 }
 
 //114
-function openPage (url) {
+function openPage(url) {
+  if (timer != null) {
+    clearTimeout(timer);
+  }
 
-    if(timer != null){
-        clearTimeout(timer);   
-    }
-
-    if(url.indexOf("?") == -1) {
-        url = url + "?";
-    }
-    var encodeUrl = encodeURI(url + "&userLoggedIn=" + userLoggedIn);
-    $("#mainContent").load(encodeUrl);
-    $("body").scrollTop(0);
-    history.pushState(null, null, url);
+  if (url.indexOf('?') == -1) {
+    url = url + '?';
+  }
+  var encodeUrl = encodeURI(url + '&userLoggedIn=' + userLoggedIn);
+  $('#mainContent').load(encodeUrl);
+  $('body').scrollTop(0);
+  history.pushState(null, null, url);
 }
 
-function removeFromPlaylist (button, playlistId) {
-    var songId = $(button).prevAll(".songId").val(); 
+function removeFromPlaylist(button, playlistId) {
+  var songId = $(button)
+    .prevAll('.songId')
+    .val();
 
-    $.post("includes/handlers/ajax/removeFromPlaylist.php", {playlistId:playlistId, songId:songId})
-    .done((error)=>{
-        if(error != "") {
-            alert(error);
-            return;
-        }
-        openPage("playlist.php?id=" + playlistId);
+  $.post('includes/handlers/ajax/removeFromPlaylist.php', {
+    playlistId: playlistId,
+    songId: songId
+  }).done(error => {
+    if (error != '') {
+      alert(error);
+      return;
+    }
+    openPage('playlist.php?id=' + playlistId);
+  });
+}
+
+function createPlaylist() {
+  var name = prompt('Please enter the name of your playlist');
+  if (alert != null) {
+    $.post('includes/handlers/ajax/createPlaylist.php', {
+      name: name,
+      username: userLoggedIn
+    }).done(error => {
+      if (error != '') {
+        alert(error);
+        return;
+      }
+      openPage('yourMusic.php');
     });
-
-}
-
-function createPlaylist () {
-    var name = prompt('Please enter the name of your playlist');
-    if(alert != null) {
-
-        $.post("includes/handlers/ajax/createPlaylist.php", {name:name, username:userLoggedIn})
-        .done((error)=>{
-            if(error != "") {
-                alert(error);
-                return;
-            }
-            openPage("yourMusic.php");
-        })
-    }
+  }
 }
 
 function deletePlaylist(playlistId) {
-    var prompt = confirm("Are you sure you want to delete this playlist?");
+  var prompt = confirm('Are you sure you want to delete this playlist?');
 
-    if(prompt) {
-        $.post("includes/handlers/ajax/deletePlaylist.php", {playlistId:playlistId})
-        .done((error)=>{
-            if(error != "") {
-                alert(error);
-                return;
-            }
-            openPage("yourMusic.php");
-        });
-    }
+  if (prompt) {
+    $.post('includes/handlers/ajax/deletePlaylist.php', {
+      playlistId: playlistId
+    }).done(error => {
+      if (error != '') {
+        alert(error);
+        return;
+      }
+      openPage('yourMusic.php');
+    });
+  }
 }
 
-function hideOptionsMenu () {
-    var menu = $(".optionsMenu");
-    if(menu.css("display") != "none") {
-        menu.css("display", "none");
-    }
+function hideOptionsMenu() {
+  var menu = $('.optionsMenu');
+  if (menu.css('display') != 'none') {
+    menu.css('display', 'none');
+  }
 }
 
-function showOptionMenu (button) {
-    var songId = $(button).prevAll(".songId").val(); // prevAll takes multiple ancestors if needed to, as opposed to prev that stops and imidiate ancestor
-    var menu = $(".optionsMenu");
-    var menuWidth = menu.width();
-    menu.find(".songId").val(songId); // takes the options menu and goes and finds the song id which is the input hidden .songId
+function showOptionMenu(button) {
+  var songId = $(button)
+    .prevAll('.songId')
+    .val(); // prevAll takes multiple ancestors if needed to, as opposed to prev that stops and imidiate ancestor
+  var menu = $('.optionsMenu');
+  var menuWidth = menu.width();
+  menu.find('.songId').val(songId); // takes the options menu and goes and finds the song id which is the input hidden .songId
 
-    var scrollTop = $(window).scrollTop(); //A distance from the top of the window to the top of a document
-    var elementOffset = $(button).offset().top; // distance from top of document
+  var scrollTop = $(window).scrollTop(); //A distance from the top of the window to the top of a document
+  var elementOffset = $(button).offset().top; // distance from top of document
 
-    var top = elementOffset - scrollTop;
-    var left = $(button).position().left;
+  var top = elementOffset - scrollTop;
+  var left = $(button).position().left;
 
-    menu.css({"top": top + "px", "left": left - menuWidth + "px", "display": "inline"});
+  menu.css({
+    top: top + 'px',
+    left: left - menuWidth + 'px',
+    display: 'inline'
+  });
 }
 
-function formatTime (seconds) {
-    var time = Math.round(seconds);
-    var minutes = Math.floor(time/60);
-    var seconds = time - minutes * 60;
+function formatTime(seconds) {
+  var time = Math.round(seconds);
+  var minutes = Math.floor(time / 60);
+  var seconds = time - minutes * 60;
 
-    var extraZero = (seconds < 10) ? "0": "";
-    // if(seconds <10 ){
-    //     extraZero="0";
-    // } else {
-    //     extraZero="";
-    // }
+  var extraZero = seconds < 10 ? '0' : '';
+  // if(seconds <10 ){
+  //     extraZero="0";
+  // } else {
+  //     extraZero="";
+  // }
 
-    return minutes + ":" + extraZero + seconds;
+  return minutes + ':' + extraZero + seconds;
 }
 
 function updateTimeProgressBar(audio) {
-    $(".progressTime.current").text(formatTime(audio.currentTime));
-    $(".progressTime.remaining").text(formatTime(audio.duration - audio.currentTime));
+  $('.progressTime.current').text(formatTime(audio.currentTime));
+  $('.progressTime.remaining').text(
+    formatTime(audio.duration - audio.currentTime)
+  );
 
-    var progress = audio.currentTime / audio.duration * 100;
-    $(".playbackBar .progress").css("width", progress + "%");
-};
-
-function updateVolumeProgressBar (audio) {
-    var volume = audio.volume * 100;
-    $(".volumeBar .progress").css("width", volume + "%");
-
+  var progress = (audio.currentTime / audio.duration) * 100;
+  $('.playbackBar .progress').css('width', progress + '%');
 }
 
-function playFirstSong () {
-    setTrack(tempPlaylist[0], tempPlaylist, true);
+function updateVolumeProgressBar(audio) {
+  var volume = audio.volume * 100;
+  $('.volumeBar .progress').css('width', volume + '%');
+}
+
+function playFirstSong() {
+  setTrack(tempPlaylist[0], tempPlaylist, true);
 }
 
 function Audio() {
+  this.currentlyPlaying;
+  this.audio = document.createElement('audio');
 
-    this.currentlyPlaying;
-    this.audio = document.createElement('audio');
+  this.audio.addEventListener('ended', function() {
+    nextSong();
+  });
 
-    this.audio.addEventListener("ended", function (){
-        nextSong();
-    });
-    
-    this.audio.addEventListener("canplay", function(){
-        var duration = formatTime(this.duration);
-        $(".progressTime.remaining").text(duration);
-    });
+  this.audio.addEventListener('canplay', function() {
+    var duration = formatTime(this.duration);
+    $('.progressTime.remaining').text(duration);
+  });
 
-    this.audio.addEventListener("timeupdate", function(){
-        if(this.duration) {
-            updateTimeProgressBar(this);
-        }
-    });
-
-    this.audio.addEventListener("volumechange", function() {
-        updateVolumeProgressBar(this);
-    })
-    this.setTrack = function(track) {
-        this.currentlyPlaying = track;
-        this.audio.src = track.path;
+  this.audio.addEventListener('timeupdate', function() {
+    if (this.duration) {
+      updateTimeProgressBar(this);
     }
+  });
 
-    this.play = function () {
-        this.audio.play();
-    }
+  this.audio.addEventListener('volumechange', function() {
+    updateVolumeProgressBar(this);
+  });
+  this.setTrack = function(track) {
+    this.currentlyPlaying = track;
+    this.audio.src = track.path;
+  };
 
-    this.pause = function () {
-        this.audio.pause();
-    }
+  this.play = function() {
+    this.audio.play();
+  };
 
-    this.setTime = function(seconds) {
-        this.audio.currentTime = seconds;
-    }
+  this.pause = function() {
+    this.audio.pause();
+  };
+
+  this.setTime = function(seconds) {
+    this.audio.currentTime = seconds;
+  };
 }
